816ced918524b88da20c572aa84e9b946272e2c9
Calond <ruben.spolmink@gmail.com>
2018-02-08 22:00:28 +0100

    Add files via upload
    speech.py this file takes sentences and pos tags them.

---
+++ speech.py
@@ -0,0 +1,17 @@
+import nltk
+sentence = input("provide sentence: ") #for the prototype a sentence is given, but can be changed to given sentence
+tokens = nltk.word_tokenize(sentence)
+tagged = nltk.pos_tag(tokens)
+store = list();
+verbs = {
+        'VB': 1,
+        'VBD': 1,
+        'VBG' : 1,
+        'VBN' : 1,
+        'VBP' : 1,
+        'VBZ' : 1
+    }
+for i in range(0,len(tagged)):
+    if (verbs.get(tagged[i][1],'0') == 1) :  
+        store.append(tagged[i][0])
+print (store)
